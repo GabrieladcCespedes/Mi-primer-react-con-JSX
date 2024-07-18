e57820456257def63159808f27caff280fde5f2f
GabrieladcCespedes <160735888+GabrieladcCespedes@users.noreply.github.com>
2024-07-18 15:36:44 -0400

    Add files via upload

---
+++ mi-primera-app/src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import Lista from './Lista';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+      <h1>Hello Dojo!</h1>
+      <h2>Things I need to do:</h2>
+      <Lista/>
+      </header>
+    </div>
+  );
+}
+
+export default App;
